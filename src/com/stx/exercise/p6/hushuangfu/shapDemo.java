de00fddd2e1c80ed24d41444fe809c6ba77d579b
“lian” <lian@itcast.cn>
2023-09-27 16:53:57 +0800

    upadte p6

--- src/com/stx/exercise/p6/hushuangfu/shapDemo.java
+++ src/com/stx/exercise/p6/hushuangfu/shapDemo.java
@@ -7,14 +7,14 @@ package com.stx.exercise.p6.hushuangfu;
  * @date 2023/9/27 15:58:39
  * @version 1.0
  */
-public class ShapDemo {
+public class shapDemo {
     public static void main(String[] args) {
-        Cycle circle = new Cycle(5.0);
-        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Shap shap = new Cycle(5.0);
+        Shap shap1 = new Rectangle(4.0, 6.0);
 
         // 计算并打印圆和矩形的面积
-        System.out.println("圆的面积：" + getShapArea(circle));
-        System.out.println("矩形的面积：" + getShapArea(rectangle));
+        System.out.println("圆的面积：" + getShapArea(shap));
+        System.out.println("矩形的面积：" + getShapArea(shap1));
 
     }
 
